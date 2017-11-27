3eae078b3e97ba0cd4746648cb0243ab5dafbc56
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-11-27 15:43:49 +0100

    add commands

--- WebBluetooth/Myo/app.js
+++ WebBluetooth/Myo/app.js
@@ -2,10 +2,13 @@
 
 // Get from https://github.com/chromakode/bicyclejs-talk/blob/07479fe3acfd722c930229d62ecadfa7ad3cdce3/src/myo.js
 
-const controlService = 'd5060001-a904-deb9-4748-2c7f4a124842';
-const gestureService = 'd5060003-a904-deb9-4748-2c7f4a124842';
-const commandCharacteristic = 'd5060401-a904-deb9-4748-2c7f4a124842';
-const gestureCharacteristic = 'd5060103-a904-deb9-4748-2c7f4a124842';
+const controlService =                  'd5060001-a904-deb9-4748-2c7f4a124842';
+const gestureService =                  'd5060003-a904-deb9-4748-2c7f4a124842';
+const myoInfoCharacteristic =           'd5060101-a904-deb9-4748-2c7f4a124842';
+const FirmwareVersionCharacteristic =   'd5060201-a904-deb9-4748-2c7f4a124842';
+const commandCharacteristic =           'd5060401-a904-deb9-4748-2c7f4a124842';
+const gestureCharacteristic =           'd5060103-a904-deb9-4748-2c7f4a124842';
+
 const enableGesturesCommand = new Uint8Array(5);
 enableGesturesCommand[0] = 0x01; // set mode
 enableGesturesCommand[1] = 0x03; // bytes in payload
@@ -20,17 +23,50 @@ const deepSleepCommand = new Uint8Array(2);
 deepSleepCommand[0] = 0x04; // set mode
 deepSleepCommand[1] = 0x00; // bytes in payload
 
+const vibrateCommand = new Uint8Array(3);
+vibrateCommand[0] = 0x03; // set mode
+vibrateCommand[1] = 0x01; // bytes in payload
+vibrateCommand[2] = 0x00; //none vibration
+
+const vibrateShortCommand = Uint8Array.from(vibrateCommand);
+vibrateShortCommand[2] = 0x01; //short vibration
+
+const vibrateMediumCommand = Uint8Array.from(vibrateCommand);
+vibrateMediumCommand[2] = 0x02; // medium vibration
+
+const vibrateLongCommand = Uint8Array.from(vibrateCommand);
+vibrateLongCommand[2] = 0x03; // long vibration
+
+const sleepCommand = new Uint8Array(3);
+sleepCommand[0] = 0x09; // set mode
+sleepCommand[1] = 0x01; // bytes in payload
+sleepCommand[2] = 0x00; // normal sleep
+
+const neverSleepCommand = Uint8Array.from(sleepCommand);
+neverSleepCommand[2] = 0x01; // never go to sleep
+
+const unlockCommand = new Uint8Array(3);
+unlockCommand[0] = 0x0a; //set mode
+unlockCommand[1] = 0x01; //bytes in payload
+unlockCommand[2] = 0x00; // Re-lock immediatly
+
+const unlockTimedCommand = Uint8Array.from(unlockCommand);
+unlockTimedCommand[2] = 0x01; // Unlock now and re-lock after a fixed timeout.
+
+const unlockHoldCommand = Uint8Array.from(unlockCommand);
+unlockHoldCommand[2] = 0x02; //  Unlock now and remain unlocked until a lock command is received.
+
+const userActionCommand = new Uint8Array(3);
+userActionCommand[0] = 0x0b; // set mode
+userActionCommand[1] = 0x01; // bytes in payload
+userActionCommand[2] = 0x00; // User did a single, discrete action, such as pausing a video.
+
 const shape = {
     filters: [
         {name: 'JefMyo'},{
         services: [controlService],
     }],
-    optionalServices: [gestureService],
-    listen: {
-        [gestureService]: [
-            gestureCharacteristic,
-        ]
-    },
+    optionalServices: [gestureService]
 };
 
 (function() {
@@ -153,8 +189,15 @@ const shape = {
                     document.querySelector('.console').textContent = 'control service retrieve';
                 const realCommandChar = await realControlService.getCharacteristic(commandCharacteristic)
                     console.info('Get command characteristic');
-                    console.info('try to write enable gesture command');
+                    console.info('try to get myo Info characteristic');
+                const realMyoInfoChar = await realControlService.getCharacteristic(myoInfoCharacteristic);
+                    console.info('Get info characteristic');
                     document.querySelector('.console').textContent += '\n command characteristic retrieve';
+                    console.info('try to get myo info values');
+                const valueMyoInfo = await realMyoInfoChar.readValue();
+                    console.info('Get info characteristic values !');
+                    showMyoInfos(valueMyoInfo);
+                    console.info('try to write enable gesture command');
                 await realCommandChar.writeValue(enableGesturesCommand)
                     console.info('Try to get Gesture Service');
                     document.querySelector('.console').textContent = 'Try to get Gesture service';
@@ -180,6 +223,35 @@ const shape = {
             }
 
         });
+    }
+
+    function showMyoInfos(value){
+        console.log(value);
+        console.log(value.buffer);
+        console.log(String.fromCharCode.apply(null, new Uint16Array(value)));
+        console.log(String.fromCharCode.apply(null, new Uint8Array(value)));
+        console.log(String.fromCharCode.apply(null, new Uint16Array(value.buffer)));
+        console.log(String.fromCharCode.apply(null, new Uint8Array(value.buffer)));
+        try{
+            let serialNumber = value.getInt8(0);
+            let unlockPose = value.getInt16(1);
+            let activeClassifierType = value.getInt8(3);
+            let activeClassifierIndex = value.getInt8(4);
+            let hasCustomClassifier = value.getInt8(5);
+            let streamIndicating = value.getInt8(6);
+            let sku = value.getInt8(7);
+
+            console.log('serialNumber', serialNumber);
+            console.log('unlockPose', unlockPose);
+            console.log('activeClassifierType', activeClassifierType, activeClassifierType === 0 ? 'classifier Package' : 'user data');
+            console.log('activeClassifierIndex', activeClassifierIndex);
+            console.log('hasCustomClassifier', hasCustomClassifier, hasCustomClassifier === 1 ? 'Valid custom classifier' : 'no valid custom classifier');
+            console.log('streamIndicating', streamIndicating);
+            console.log('sku', sku, sku === 0 ? 'Unkown Myo' : sku === 1 ? 'Black Myo' : 'White Myo');
+
+        }catch(e){
+            console.error(e);
+        }
     }
 
     window.addEventListener('load', pageLoad);
