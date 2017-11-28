89acd7abf4aeb8a4b95adb007978f6b8718858df
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-11-28 16:41:34 +0100

    refactor code to add more control with buttons

--- WebBluetooth/Myo/app.js
+++ WebBluetooth/Myo/app.js
@@ -2,23 +2,23 @@
 
 // Get from https://github.com/chromakode/bicyclejs-talk/blob/07479fe3acfd722c930229d62ecadfa7ad3cdce3/src/myo.js
 
-const controlService =                  'd5060001-a904-deb9-4748-2c7f4a124842';
-const myoInfoCharacteristic =           'd5060101-a904-deb9-4748-2c7f4a124842';
-const firmwareVersionCharacteristic =   'd5060201-a904-deb9-4748-2c7f4a124842';
-const commandCharacteristic =           'd5060401-a904-deb9-4748-2c7f4a124842';
+const controlServiceUID =                  'd5060001-a904-deb9-4748-2c7f4a124842';
+const myoInfoCharacteristicUID =           'd5060101-a904-deb9-4748-2c7f4a124842';
+const firmwareVersionCharacteristicUID =   'd5060201-a904-deb9-4748-2c7f4a124842';
+const commandCharacteristicUID =           'd5060401-a904-deb9-4748-2c7f4a124842';
 
-const gestureService =                  'd5060003-a904-deb9-4748-2c7f4a124842';
-const gestureCharacteristic =           'd5060103-a904-deb9-4748-2c7f4a124842';
+const gestureServiceUID =                  'd5060003-a904-deb9-4748-2c7f4a124842';
+const gestureCharacteristicUID =           'd5060103-a904-deb9-4748-2c7f4a124842';
 
-const ImuService =                      'd5060002-a904-deb9-4748-2c7f4a124842';
-const IMUDataCharacteristic =           'd5060402-a904-deb9-4748-2c7f4a124842';
-const MotionEventCharacteristic =       'd5060502-a904-deb9-4748-2c7f4a124842';
+const ImuDataServiceUID =                  'd5060002-a904-deb9-4748-2c7f4a124842';
+const IMUDataCharacteristicUID =           'd5060402-a904-deb9-4748-2c7f4a124842';
+const MotionEventCharacteristicUID =       'd5060502-a904-deb9-4748-2c7f4a124842';
 
-const EmgDataService =                  'd5060005-a904-deb9-4748-2c7f4a124842';
-const EmgData0Characteristic =          'd5060105-a904-deb9-4748-2c7f4a124842';
-const EmgData1Characteristic =          'd5060205-a904-deb9-4748-2c7f4a124842';
-const EmgData2Characteristic =          'd5060305-a904-deb9-4748-2c7f4a124842';
-const EmgData3Characteristic =          'd5060405-a904-deb9-4748-2c7f4a124842';
+const EmgDataServiceUID =                  'd5060005-a904-deb9-4748-2c7f4a124842';
+const EmgData0CharacteristicUID =          'd5060105-a904-deb9-4748-2c7f4a124842';
+const EmgData1CharacteristicUID =          'd5060205-a904-deb9-4748-2c7f4a124842';
+const EmgData2CharacteristicUID =          'd5060305-a904-deb9-4748-2c7f4a124842';
+const EmgData3CharacteristicUID =          'd5060405-a904-deb9-4748-2c7f4a124842';
 
 const enableGesturesCommand = new Uint8Array(5);
 enableGesturesCommand[0] = 0x01; // set mode
@@ -75,176 +75,178 @@ userActionCommand[2] = 0x00; // User did a single, discrete action, such as paus
 const shape = {
     filters: [
         {name: 'JefMyo'},{
-        services: [controlService],
+        services: [controlServiceUID],
     }],
-    optionalServices: [gestureService]
+    optionalServices: [gestureServiceUID]
 };
 
 (function() {
 
-    let currentDevice = null;
+    let device = null;
+    let gattServer = null;
+
+    let controlService = null;
+    let myoInfoCharacteristic = null;
+    let firmwareVersionCharacteristic = null;
+    let commandCharacteristic = null;
+
+    let gestureService = null;
+    let gestureCharacteristic = null;
+
+    let ImuDataService = null;
+    let IMUDataCharacteristic = null;
+    let MotionEventCharacteristic = null;
+
+    let EmgDataService = null;
+    let EmgData0Characteristic = null;
+    let EmgData1Characteristic = null;
+    let EmgData2Characteristic = null;
+    let EmgData3Characteristic = null;
 
     function parseMyoGesture(value) {
-        if (value.getUint8(0) === 0x03) {
-            const gestureValue = value.getUint16(1, true)
-            const gesture = {
-                0x0000: 'rest',
-                0x0001: 'fist',
-                0x0002: 'wave-in',
-                0x0003: 'wave-out',
-                0x0004: 'fingers-spread',
-                0x0005: 'double-tap',
-                0xffff: 'unknown',
-            }[gestureValue]
-            return { gesture }
+        console.log('ParseMyoGesture');
+        switch(value.getUint8(0)){
+            case 0x01: // arm_sync
+                console.log('arm_sync');
+                break;
+            case 0x02: // arm_unsync
+                console.log('arm_unsync');
+                break;
+            case 0x03: // pose
+                const gestureValue = value.getUint16(1, true)
+                const gesture = {
+                    0x0000: 'rest',
+                    0x0001: 'fist',
+                    0x0002: 'wave-in',
+                    0x0003: 'wave-out',
+                    0x0004: 'fingers-spread',
+                    0x0005: 'double-tap',
+                    0xffff: 'unknown',
+                }[gestureValue]
+                return { gesture }
+            case 0x04: // unlock
+                console.log('unlock');
+                break;
+            case 0x05: // locked
+                console.log('locked');
+                break;
+            case 0x06: // sync_failed
+                console.log('sync_failed');
+                break;
+            case 0x07: // warmup
+                console.log('warmup');
+                break;
+            default:
+                console.log('Nothing !');
         }
+
+
         return { gesture: null }
     }
 
     function pageLoad() {
-        document.getElementById('connect').addEventListener('click', () => {
-            devicePromise = navigator.bluetooth.requestDevice(shape);
 
-            gattPromise = devicePromise.then(function(device) {
-                    document.querySelector('.console').textContent = 'connecting...';
-                    currentDevice = device;
-                    return device.gatt.connect();
-                });
 
-                /*.then((server) => {
-                    document.querySelector('.console').textContent += '\n server connected';
-                    return currentDevice.gatt.getPrimaryService(controlService)
-                });*/
-        });
-        document.getElementById('getControlService').addEventListener('click', ()=>{
-            gestureServicePromise = gattPromise.then(gatt => {
-                document.querySelector('.console').textContent = 'Try to get Gesture';
-                return gatt.getPrimaryService(gestureService)
-            });
-        });
-
-        document.getElementById('getChar').addEventListener('click', ()=>{
-            gestureServicePromise.then(service=>{
-                document.querySelector('.console').textContent = 'Try to get characteristic';
-                return service.getCharacteristic(gestureCharacteristic)
-            });
-
-        });
-        document.getElementById('all').addEventListener('click', ()=>{
-            navigator.bluetooth.requestDevice(shape)
-                .then(function(device) {
-                    console.info('Get Device, ', device);
-                    console.info('Try to connecting');
-                    document.querySelector('.console').textContent = 'connecting...';
-                    currentDevice = device;
-                    return device.gatt.connect();
-                })
-                .then(()=>{
-                    console.info('Connect to myo device');
-                    console.info('Try to get Control Service');
-                    document.querySelector('.console').textContent = 'Try to get Control service';
-                    return currentDevice.gatt.getPrimaryService(controlService)
-                })
-                .then((service) => {
-                    console.info('Get Control Service');
-                    console.info('try to get command characteristic');
-                    document.querySelector('.console').textContent = 'control service retrieve';
-                    return service.getCharacteristic(commandCharacteristic)
-                })
-                .then((characteristic) => {
-                    console.info('Get command characteristic');
-                    console.info('try to write enable gesture command');
-                    document.querySelector('.console').textContent += '\n command characteristic retrieve';
-                    return characteristic.writeValue(enableGesturesCommand)
-                })
-                .then(() => {
-                    console.info('Try to get Gesture Service');
-                    document.querySelector('.console').textContent = 'Try to get Gesture service';
-                    return currentDevice.gatt.getPrimaryService(gestureService)
-                })
-                .then(service=>{
-                    console.info('Get Gesture Service');
-                    console.info('try to get Gesture characteristic');
-                    document.querySelector('.console').textContent = 'Try to get Gesture characteristic';
-                    return service.getCharacteristic(gestureCharacteristic)
-                })
-                .then((characteristic) => {
-                    console.info('Get gesture caracteristic');
-                    console.info('try to listen gestures !');
-                	document.querySelector('.console').textContent += '\ncharacteristic retrieve';
-                    characteristic.startNotifications();
-                    characteristic.addEventListener('characteristicvaluechanged', (ev) => {
-                        const gesture = parseMyoGesture(ev.target.value);
-                        console.info('Gesture : ', gesture);
-                        document.querySelector('.console').textContent = `Gesture : ${gesture.gesture}`;
-                    })
-                }).catch(function(error) {
-                    console.error(error);
-                    document.querySelector('.console').textContent = `Error ! `;
-                });
-        });
-        document.getElementById('allAsync').addEventListener('click', async ()=>{
+         document.getElementById('allAsync').addEventListener('click', async ()=>{
             try{
-                const device = await navigator.bluetooth.requestDevice(shape);
+                device = await navigator.bluetooth.requestDevice(shape);
 
                 console.info('Get Device, ', device);
                 console.info('Try to connecting');
                 document.querySelector('.console').textContent = 'connecting...';
-                const server = await device.gatt.connect();
-                    console.info('Connect to myo device');
-                    console.info('Try to get Control Service');
-                    document.querySelector('.console').textContent = 'Try to get Control service';
-                const realControlService = await server.getPrimaryService(controlService)
-                    console.info('Get Control Service');
-                    document.querySelector('.console').textContent = 'control service retrieve';
-                    // General informations about Myo
-                    console.info('try to get myo Info characteristic');
-                const realMyoInfoChar = await realControlService.getCharacteristic(myoInfoCharacteristic);
-                    console.info('Get info characteristic');
-                    console.info('try to get myo info values');
-                const valueMyoInfo = await realMyoInfoChar.readValue();
-                    console.info('Get info characteristic values !');
-                    showMyoInfos(valueMyoInfo);
-                    console.info('try to get myo firmware');
-                const realMyoFirmware = await realControlService.getCharacteristic(firmwareVersionCharacteristic);
-                    console.info('Get Firmware version characteristic');
-                    console.info('try to get myo firmware value');
-                const valueMyoFirmWare = await realMyoFirmware.readValue();
-                    console.info('Get firmware characteristic values !');
-                    showFirmwareVersion(valueMyoFirmWare);
+                gattServer = await device.gatt.connect();
+                console.info('Connect to myo device');
+                document.querySelector('.console').textContent = 'Try to read myo infos';
+                await readMyoInfos();
 
-                    // Instructions to set commands
-                    console.info('try to get command characteristic');
-                const realCommandChar = await realControlService.getCharacteristic(commandCharacteristic)
-                    console.info('Get command characteristic');
-                    document.querySelector('.console').textContent += '\n command characteristic retrieve';
-                    console.info('try to write enable gesture command');
-                await realCommandChar.writeValue(disableGesturesCommand);
-                await realCommandChar.writeValue(enableGesturesCommand);
-                    console.info('Try to get Gesture Service');
-                    document.querySelector('.console').textContent = 'Try to get Gesture service';
-                const realGestureService = await server.getPrimaryService(gestureService)
-                    console.info('Get Gesture Service');
-                    console.info('try to get Gesture characteristic');
-                    document.querySelector('.console').textContent = 'Try to get Gesture characteristic';
-                const realGestureChar = await realGestureService.getCharacteristic(gestureCharacteristic)
-                    console.info('Get gesture caracteristic');
-                    console.info('try to listen gestures !');
-                    document.querySelector('.console').textContent += '\n characteristic retrieve';
-                await realGestureChar.startNotifications();
-                    console.info('Notifications starts !');
-                    document.querySelector('.console').textContent += '\n Notifications starts ! ';
-                realGestureChar.addEventListener('characteristicvaluechanged', (ev) => {
-                        const gesture = parseMyoGesture(ev.target.value);
-                        console.info('Gesture : ', gesture);
-                        document.querySelector('.console').textContent = `Gesture : ${gesture.gesture}`;
-                    })
             }catch(error){
                 console.error(error);
                 document.querySelector('.console').textContent = `Error ! `;
             }
 
         });
+
+        document.getElementById('enableGestureCommand').addEventListener('click', async () =>{
+            try{
+                console.info('try to write enable gesture command');
+                document.querySelector('.console').textContent = 'Try to write enable gesture command';
+                await commandCharacteristic.writeValue(enableGesturesCommand);
+                console.info('command send !');
+                document.querySelector('.console').textContent += '\n Command send';
+            }catch(e){
+                console.error(e);
+                document.querySelector('.console').textContent = `Error ! `;
+            }
+        });
+
+        document.getElementById('disableGestureCommand').addEventListener('click', async () =>{
+            try{
+                console.info('try to write disable gesture command');
+                document.querySelector('.console').textContent = 'Try to write disable gesture command';
+                await commandCharacteristic.writeValue(disableGesturesCommand);
+                console.info('command send !');
+                document.querySelector('.console').textContent += '\n Command send';
+            }catch(e){
+                console.error(e);
+                document.querySelector('.console').textContent = `Error ! `;
+            }
+        });
+
+        document.getElementById('getInfos').addEventListener('click', readMyoInfos);
+        document.getElementById('readGestureCharacteristic').addEventListener('click', readGesturesAndNotifications);
+    }
+
+    async function readGesturesAndNotifications(){
+        if (! gestureService){
+            gestureService= await gattServer.getPrimaryService(gestureServiceUID)
+        }
+        console.info('Get Gesture Service');
+        console.info('try to get Gesture characteristic');
+        document.querySelector('.console').textContent = 'Try to get Gesture characteristic';
+        gestureCharacteristic = await gestureService.getCharacteristic(gestureCharacteristicUID)
+        console.info('Get gesture caracteristic');
+        console.info('try to read value classifier');
+        parseMyoGesture(await gestureCharacteristic.readValue());
+        console.info('try to listen gestures !');
+        document.querySelector('.console').textContent += '\n characteristic retrieve';
+        await gestureCharacteristic.startNotifications();
+        console.info('Notifications starts !');
+        document.querySelector('.console').textContent += '\n Notifications starts ! ';
+        gestureCharacteristic.addEventListener('characteristicvaluechanged', (ev) => {
+            const gesture = parseMyoGesture(ev.target.value);
+            console.info('Gesture : ', gesture);
+            document.querySelector('.console').textContent = `Gesture : ${gesture.gesture}`;
+        });
+    }
+
+    async function readMyoInfos(){
+        if (!controlService){
+            controlService =  await gattServer.getPrimaryService(controlServiceUID)
+        }
+        console.info('Get Control Service');
+        document.querySelector('.console').textContent = 'control service retrieve';
+
+        // General informations about Myo
+        console.info('try to get myo Info characteristic');
+        myoInfoCharacteristic = await controlService.getCharacteristic(myoInfoCharacteristicUID);
+        console.info('Get info characteristic');
+        console.info('try to get myo info values');
+        const valueMyoInfo = await myoInfoCharacteristic.readValue();
+        console.info('Get info characteristic values !');
+        showMyoInfos(valueMyoInfo);
+        console.info('try to get myo firmware');
+        firmwareVersionCharacteristic = await controlService.getCharacteristic(firmwareVersionCharacteristicUID);
+        console.info('Get Firmware version characteristic');
+        console.info('try to get myo firmware value');
+        const valueMyoFirmWare = await firmwareVersionCharacteristic.readValue();
+        console.info('Get firmware characteristic values !');
+        showFirmwareVersion(valueMyoFirmWare);
+
+        // Instructions to set commands
+        console.info('try to get command characteristic');
+        commandCharacteristic = await controlService.getCharacteristic(commandCharacteristicUID)
+        console.info('Get command characteristic');
+        document.querySelector('.console').textContent += '\n command characteristic retrieve';
     }
 
     function showFirmwareVersion(value){
